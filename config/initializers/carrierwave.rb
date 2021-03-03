78e470dfdbac1356b293f4bd16793c4d0f05f9d6
root <root@LAPTOP-H86GJAI6.localdomain>
2021-03-03 11:25:10 +0000

    Fixed aws key references

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,9 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     Rails.application.credentials.aws.aws_public_key,                        # required unless using use_iam_profile
-      aws_secret_access_key: Rails.application.credentials.aws.aws_secret_key,                        # required unless using use_iam_profile
-      use_iam_profile:       true,                         # optional, defaults to false
-      region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-      host:                  's3.example.com',             # optional, defaults to nil
-      endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+      aws_access_key_id:     Rails.application.credentials.aws[Rails.env.to_sym][:aws_public_key],                        # required unless using use_iam_profile
+      aws_secret_access_key: Rails.application.credentials.aws[Rails.env.to_sym][:aws_secret_key],                        # required unless using use_iam_profile
+      region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = Rails.application.credentials.aws.aws_bucket                                      # required
+    config.fog_directory  = Rails.application.credentials.aws[Rails.env.to_sym][:aws_bucket]                                      # required
   end
