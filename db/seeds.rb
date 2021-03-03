b93c1db14674336f23abf4e2d9c89e8b37339af0
root <root@LAPTOP-H86GJAI6.localdomain>
2021-03-03 12:18:00 +0000

    Added new admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'tamtzekin@gmail.com', password: 'password', password_confirmation: 'password')
-# if Rails.env.development?
+AdminUser.create!(email: 'orchideyez@yahoo.com', password: 'password', password_confirmation: 'password')
